9d92ad9713e3e3edcea229a6d0fefb6660910e2d
cfavre <colin@cardastation.com>
2023-02-25 23:53:44 +0100

    Add (really) simple collision response between enemies

--- Assets/Scripts/Systems/CollisionSystem.cs
+++ Assets/Scripts/Systems/CollisionSystem.cs
@@ -33,12 +33,31 @@ public partial struct CollisionSystem : ISystem
         NativeArray<Entity> enemies = state.EntityManager.CreateEntityQuery(typeof(EnemyData)).ToEntityArray(Allocator.Temp);
         EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.Temp);
 
-        foreach (Entity enemy in enemies)
+        for (int i = 0; i < enemies.Length; ++i)
         {
+            Entity enemy = enemies[i];
             EnemyData enemyData = state.EntityManager.GetComponentData<EnemyData>(enemy);
+            bool collided = false;
+
+            for (int j = i + 1; j < enemies.Length; ++j)
+            {
+                Entity enemy2 = enemies[j];
+                if (SphereSphereCollision(ref state, enemy, enemy2))
+                {
+                    CollisionResponse(ref state, ref ecb, enemy, enemy2);
+                    collided = true;
+                    break;
+                }
+            }
+
+            if (collided)
+            {
+                continue;
+            }
+
             foreach (Entity bullet in bullets)
             {
-                if (SphereSphereCollision(bullet, enemy, ref state))
+                if (SphereSphereCollision(ref state, bullet, enemy))
                 {
                     ecb.DestroyEntity(bullet);
 
@@ -54,12 +73,19 @@ public partial struct CollisionSystem : ISystem
                             break;
                     }
                     Game.instance.AddToScore(enemyData.score);
+                    collided = true;
+                    break;
                 }
+            }
+
+            if (collided)
+            {
+                continue;
             }
 
             if (!playerRW.IsInvulnerable(SystemAPI.Time.ElapsedTime))
             {
-                if (SphereSphereCollision(playerEntity, enemy, ref state))
+                if (SphereSphereCollision(ref state, playerEntity, enemy))
                 {
                     playerRW.SetDead(true);
                 }
@@ -71,7 +97,7 @@ public partial struct CollisionSystem : ISystem
             PowerUpData powerUpData = state.EntityManager.GetComponentData<PowerUpData>(powerUp);
             if (!playerRW.IsDead)
             {
-                if (SphereSphereCollision(playerEntity, powerUp, ref state))
+                if (SphereSphereCollision(ref state, playerEntity, powerUp))
                 {
                     playerRW.PickupPowerUp(powerUpData.type, SystemAPI.Time.ElapsedTime, powerUpData.duration);
                     ecb.DestroyEntity(powerUp);
@@ -87,13 +113,36 @@ public partial struct CollisionSystem : ISystem
     }
 
     [BurstCompile]
-    public bool SphereSphereCollision(Entity entityA, Entity entityB, ref SystemState state)
+    public bool SphereSphereCollision(ref SystemState state, Entity entityA, Entity entityB)
     {
-        Vector3 posA = state.EntityManager.GetComponentData<LocalTransform>(entityA).Position;
+        float3 posA = state.EntityManager.GetComponentData<LocalTransform>(entityA).Position;
         float radiusA = state.EntityManager.GetComponentData<SphereColliderData>(entityA).radius;
-        Vector3 posB = state.EntityManager.GetComponentData<LocalTransform>(entityB).Position;
+        float3 posB = state.EntityManager.GetComponentData<LocalTransform>(entityB).Position;
         float radiusB = state.EntityManager.GetComponentData<SphereColliderData>(entityB).radius;
         float radiusSum = radiusA + radiusB;
         return math.distancesq(posA, posB) <= radiusSum * radiusSum;
+    }
+
+    [BurstCompile]
+    public void CollisionResponse(ref SystemState state, ref EntityCommandBuffer ecb, Entity entityA, Entity entityB)
+    {
+        float3 posA = state.EntityManager.GetComponentData<LocalTransform>(entityA).Position;
+        float3 posB = state.EntityManager.GetComponentData<LocalTransform>(entityB).Position;
+        MovementData movementA = state.EntityManager.GetComponentData<MovementData>(entityA);
+        MovementData movementB = state.EntityManager.GetComponentData<MovementData>(entityB);
+        float3 AtoB = posB - posA;
+        float dist = math.length(AtoB);
+        float3 AtoBNormal = AtoB/ dist;
+
+        if (math.dot(movementA.velocity, AtoBNormal) > 0)
+        {
+            movementA.velocity -= 2.0f * math.dot(movementA.velocity, AtoBNormal) * AtoBNormal;
+            ecb.SetComponent(entityA, movementA);
+        }
+        if (math.dot(movementB.velocity, -AtoBNormal) > 0)
+        {
+            movementB.velocity -= 2.0f * math.dot(movementB.velocity, AtoBNormal) * AtoBNormal;
+            ecb.SetComponent(entityB, movementB);
+        }
     }
 }
